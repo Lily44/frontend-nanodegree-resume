568e18b54e80358b23a6cc2d9772745745b93d4c
Lori Olson <lsolson@mac.com>
2016-05-19 14:03:06 -0500

    Add files via upload

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,41 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Lori ");
+
+	// 	var firstname = "Lori";
+	// 	var age = 46;
+
+	// 	var awesomeThoughts = "I am Lori and I am awesome!";
+
+	// 	var email = "lsolson@mac.com";
+
+	// 	var newEmail = 
+	// 		email.replace("mac", "gmail");
+
+	// 	var funThoughts =
+	// 		awesomeThoughts.replace("awesome", "fun");
+
+	// 	$("#main").append("<br>" + funThoughts);
+// var udacityizer = function(s) {  
+	// 	    var s =
+	// 	    s.slice(1).replace("u", "U");
+		       
+	// 	    return s;
+	// 		};
+	// 		console.log(udacityizer(s));
+	// 		$("#header").append(udacityizer(s));var s = "audacity";
+
+
+
+
+// var name = "Lori Olson"
+// var role = "Front End Developer"
+
+// var formattedName = 
+// 	HTMLheaderName.replace("%data%", name);
+
+// var formattedRole = 
+// 	HTMLheaderRole.replace("%data%", role);
+
+// $("#header").prepend(formattedName + formattedRole);
+
+
+	
